faec56931a0c1180c353dcecc233201199124195
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-13 18:30:44 +1100

    Update Contract1.sol

--- Contract1.sol
+++ Contract1.sol
@@ -1,1 +1,41 @@
+pragma solidity ^0.5.1;
 
+contract TrollFactory {
+    
+    uint public total_trolls;
+    uint public troll_types;
+    address public contract_owner;
+    bool public contract_active;
+    
+    struct Troll {
+        string name;
+        bytes4 type_of;
+        uint id;
+        uint health;
+        uint lifes;
+        uint strength;
+        uint power;
+        uint speed;
+        uint agility;
+        uint cleverness;
+        address owner;
+        bool playable;
+    }
+    
+    mapping (address => mapping(uint => Troll)) public troll_owner;
+    
+    Troll[] public trolls;
+    
+    constructor () public {
+        total_trolls = 0;
+        total_trolls = 10;
+        contract_owner = msg.sender;
+        contract_active = true;
+    }
+    
+    function Newtroll(string memory _name) public {
+        Troll memory newTroll = Troll(_name, 0, 1, 0, 0 ,0, 0, 0, 0, 0, msg.sender, true);
+        troll_owner[msg.sender][total_trolls] = newTroll;
+    }
+    
+}
