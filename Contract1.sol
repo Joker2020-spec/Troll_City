ef46b459ca1ec2cf07a8b94bf3fa4f867d7101fa
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-13 18:34:03 +1100

    Update Contract1.sol

--- Contract1.sol
+++ Contract1.sol
@@ -18,6 +18,7 @@ contract TrollFactory {
         uint speed;
         uint agility;
         uint cleverness;
+        uint troll_number;
         address owner;
         bool playable;
     }
@@ -28,14 +29,18 @@ contract TrollFactory {
     
     constructor () public {
         total_trolls = 0;
-        total_trolls = 10;
+        troll_types = 10;
         contract_owner = msg.sender;
         contract_active = true;
     }
     
     function Newtroll(string memory _name) public {
-        Troll memory newTroll = Troll(_name, 0, 1, 0, 0, 0, 0, 0, 0, 0, msg.sender, true);
+        Troll memory newTroll = Troll(_name, 0, 1, 0, 0, 0, 0, 0, 0, 0, total_trolls, msg.sender, true);
         troll_owner[msg.sender][total_trolls] = newTroll;
+    }
+    
+    function plus1() private {
+        total_trolls = total_trolls + 1;
     }
     
 }
