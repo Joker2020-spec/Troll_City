18efa8f4a48c94242a3c7e8e92491e2bf68f0404
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-14 16:17:52 +1100

    Update Contract2.sol

--- Contract2.sol
+++ Contract2.sol
@@ -4,13 +4,16 @@ import"./Contract1.sol";
 
 contract PlayerFactory is TrollFactory {
     
+    uint private Troll_Id = 0;
+    
     struct PlayerCache {
         string name;
-        Troll[] type1;
-        Troll[] type2;
-        Troll[] type3;
-        Troll[] type4;
-        Troll[] type5;
+        uint type1;
+        uint type2;
+        uint type3;
+        uint type4;
+        uint type5;
+        mapping (uint => address) player_trolls;
     }
     
     PlayerCache[] public players;
@@ -18,12 +21,22 @@ contract PlayerFactory is TrollFactory {
     mapping (address => PlayerCache) public playerProfile;
     
     function NewPlayer(string memory _name) public {
-        
         PlayerCache memory newPlayer = PlayerCache(_name, 
-            playerProfile[msg.sender].type1.push(), 
-            playerProfile[msg.sender].type2.push(0), 
-            playerProfile[msg.sender].type3.push(0), 
-            playerProfile[msg.sender].type4.push(0), 
-            playerProfile[msg.sender].type5.push(0));
+            playerProfile[msg.sender].type1 + Troll_Id, 
+            playerProfile[msg.sender].type2 + Troll_Id, 
+            playerProfile[msg.sender].type3 + Troll_Id, 
+            playerProfile[msg.sender].type4 + Troll_Id, 
+            playerProfile[msg.sender].type5 + Troll_Id
+        );
+        players.push(newPlayer);
+    }
+    
+    function NewTroll(uint _trollId) public returns (bool success) {
+        uint captured = trolls.length; 
+        for (uint i = 0; i < trolls.length; i++) {
+            if (trolls.length[i] == troll_owner[msg.sender][_trollId].troll_number) {
+                
+            }
+        }
     }
 }
