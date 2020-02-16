d3a73e81edda8f7012f5045cedc18e258619e46d
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-16 14:10:25 +1100

    Update Contract2.sol

--- Contract2.sol
+++ Contract2.sol
@@ -1,15 +1,21 @@
 pragma solidity 0.5.1;
 
 import"./Contract1.sol";
+import"./SafeMath.sol";
 
 contract PlayerFactory is TrollFactory {
     
-    uint256 private Troll_Id = 0;
+    using SafeMath for uint256;
+    
     uint256 private Min_Payment = 0.1 ether;
+    uint256 payment2 = 0.25 ether;
+    uint256 payment3 = 0.5 ether;
+    uint256 payment4 = 0.75 ether;
     uint256 private Max_Payment = 1 ether;
     
     struct PlayerCache {
         string name;
+        address key;
         uint256 type1;
         uint256 type2;
         uint256 type3;
@@ -23,19 +29,28 @@ contract PlayerFactory is TrollFactory {
     mapping (address => PlayerCache) public playerProfile;
     
     function NewPlayer(string memory _name) public {
-        PlayerCache memory newPlayer = PlayerCache(_name, 
-            playerProfile[msg.sender].type1 + Troll_Id, 
-            playerProfile[msg.sender].type2 + Troll_Id, 
-            playerProfile[msg.sender].type3 + Troll_Id, 
-            playerProfile[msg.sender].type4 + Troll_Id, 
-            playerProfile[msg.sender].type5 + Troll_Id
-        );
+        PlayerCache memory newPlayer = PlayerCache(_name, msg.sender, 0, 0, 0, 0, 0);
         players.push(newPlayer);
     }
     
     function NewTroll(string memory _name) public payable returns (bool success) {
-        Newtroll_T1(_name);
-        playerProfile[msg.sender].type1 + 1;
+        if (msg.value == Min_Payment) {
+            Newtroll_T1(_name);
+            playerProfile[msg.sender].type1.add(1);
+        } else if (msg.value == payment2) {
+            Newtroll_T2(_name);
+            playerProfile[msg.sender].type2.add(1);
+        } else if (msg.value == payment3) {
+            Newtroll_T3(_name);
+            playerProfile[msg.sender].type3.add(1);
+        } else if (msg.value == payment4) {
+            Newtroll_T4(_name);
+            playerProfile[msg.sender].type4.add(1);
+        } else if (msg.value == Max_Payment) {
+            Newtroll_T5(_name);
+            playerProfile[msg.sender].type5.add(1);
+        }
+        return success;
         
     }
     
