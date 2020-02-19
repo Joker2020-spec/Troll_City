d3fe986654e99a3bf901159536af3875987afdfd
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-19 15:43:56 +1100

    Update Contract4.sol

--- Contract4.sol
+++ Contract4.sol
@@ -4,30 +4,41 @@ import"./Contract2.sol";
 
 contract PlayerInteraction is PlayerFactory {
     
+    uint256 games_open = 0;
+    uint256 games_finished = 0;
     uint256 games_played = 0;
     
-    enum SportsGames {FOOTBALL, BASKETBALL, BASEBALL, GOLF}
-    enum  {SINGLES, TEAMS}
+    enum SportsGames {FOOTBALL, BASKETBALL, BASEBALL, GOLF} SportsGames SG;
+    enum GameChoice {SINGLES, TEAMS} GameChoice GC;
+
     
     struct Game {
         address player1;
         address player2;
         uint score1;
         uint score2;
-        SportsGames 
+        SportsGames sg;
+        GameChoice gc;
         bool begun;
         bool finished;
-        
     }
-
     
-    function SelectGame() public {
-        
+    Game[] public game_on;
+    
+    
+    function OpenGame(SportsGames _SG, GameChoice _GC, uint _gameNum) public {
+        game_on[_gameNum].sg = _SG;
+        game_on[_gameNum].gc = _GC;
+        games_open++;
     }
     
     function GamePlayed() public returns (bool finished) {
         games_played++;
         return true;
+    }
+    
+    function Choice(uint _SG, uint _GC) internal {
+        
     }
     
     
