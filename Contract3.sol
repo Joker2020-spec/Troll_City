781594bee015b62d886358d08b374cdcb84cfe70
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-21 16:12:54 +1100

    Update Contract3.sol

--- Contract3.sol
+++ Contract3.sol
@@ -1,99 +1,113 @@
-pragma solidity 0.5.1;
+pragma solidity ^0.5.1;
 
 import"./Contract1.sol";
-import"./SafeMath.sol";
 
-contract PlayerFactory2 is TrollFactory {
+
+contract TrollStatFactory is TrollFactory {
     
-    using SafeMath for uint256;
     
-    uint256 private payment1 = 0.1 ether;
-    uint256 private payment2 = 0.25 ether;
-    uint256 private payment3 = 0.5 ether;
-    uint256 private payment4 = 0.75 ether;
-    uint256 private payment5 = 1 ether;
-    
-    struct PlayerCache {
-        string name;
-        address key;
-        uint256 id;
-        uint256 type1;
-        uint256 type2;
-        uint256 type3;
-        uint256 type4;
-        uint256 type5;
-        uint256[] player_trolls;
-        bool active;
+    function IncreaseHealth(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].health = trolls[_trollNumber].health.add(_amount);
+        troll_owner[_key][_trollNumber].health = troll_owner[_key][_trollNumber].health.add(_amount);
+        return trolls[_trollNumber].health;
     }
     
-    PlayerCache[] public players;
-    
-    mapping (address => PlayerCache) public playerProfile;
-    
-    function NewPlayer(string memory _name) public {
-        uint256[] memory list;
-        PlayerCache memory newPlayer = PlayerCache({name: _name, key: msg.sender, id: players.length, type1: 0, type2: 0, type3: 0, type4: 0, type5: 0, player_trolls: list, active: true});
-        playerProfile[msg.sender] = newPlayer;
-        players.push(newPlayer);
+    function IncreaseLifes(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].lifes = trolls[_trollNumber].lifes.add(_amount);
+        troll_owner[_key][_trollNumber].lifes = troll_owner[_key][_trollNumber].lifes.add(_amount);
+        return trolls[_trollNumber].lifes;
     }
     
-    function UpdateType(uint256 _playerId, uint256 _trollId, address _key) public {
-        require (playerProfile[_key].key == _key);
-        require (troll_owner[_key][_trollId].owner == _key);
-        if (trolls[_trollId].troll_level == )
+    function IncreaseStrength(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].strength = trolls[_trollNumber].strength.add(_amount);
+        troll_owner[_key][_trollNumber].strength = troll_owner[_key][_trollNumber].strength.add(_amount);
+        return trolls[_trollNumber].strength;
     }
     
-    function NewTrollType1(string memory _name) public payable returns (bool success) {
-        CheckPayment;
-        if (msg.value == payment1) {
-            Newtroll_T1(_name);
-            UpdateTypeOne();
-            return true;
-        } else {
-            revert("Payment is not valid");
-        }
-        
+    function IncreasePower(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].power = trolls[_trollNumber].power.add(_amount);
+        troll_owner[_key][_trollNumber].power = troll_owner[_key][_trollNumber].power.add(_amount);
+        return trolls[_trollNumber].power;
     }
     
-    function NewTrollType2(string memory _name) public payable returns (bool success) {
-        CheckPayment;
-        if (msg.value == payment2) {
-            Newtroll_T2(_name);
-            UpdateTypeTwo();
-        } else {
-            revert("Payment is not valid");
+    function IncreaseSpeed(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].speed = trolls[_trollNumber].speed.add(_amount);
+        troll_owner[_key][_trollNumber].speed = troll_owner[_key][_trollNumber].speed.add(_amount);
+        return trolls[_trollNumber].speed;
+    }
+    
+    function IncreaseAgility(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].agility = trolls[_trollNumber].agility.add(_amount);
+        troll_owner[_key][_trollNumber].agility = troll_owner[_key][_trollNumber].agility.add(_amount);
+        return trolls[_trollNumber].agility;
+    }
+    
+    function IncreaseCleverness(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].cleverness = trolls[_trollNumber].cleverness.add(_amount);
+        troll_owner[_key][_trollNumber].cleverness = troll_owner[_key][_trollNumber].cleverness.add(_amount);
+        return trolls[_trollNumber].cleverness;
+    }
+    
+    function DecreaseHealth(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].health = trolls[_trollNumber].health.sub(_amount);
+        troll_owner[_key][_trollNumber].health = troll_owner[_key][_trollNumber].health.sub(_amount);
+        return trolls[_trollNumber].health;
+    }
+    
+    function DecreaseLifes(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].lifes = trolls[_trollNumber].lifes.sub(_amount);
+        troll_owner[_key][_trollNumber].lifes = troll_owner[_key][_trollNumber].lifes.sub(_amount);
+        return trolls[_trollNumber].lifes;
+    }
+    
+    function DecreaseStrength(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].strength = trolls[_trollNumber].strength.sub(_amount);
+        troll_owner[_key][_trollNumber].strength = troll_owner[_key][_trollNumber].strength.sub(_amount);
+        return trolls[_trollNumber].strength;
+    }
+    
+    function DecreasePower(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].power = trolls[_trollNumber].power.sub(_amount);
+        troll_owner[_key][_trollNumber].power = troll_owner[_key][_trollNumber].power.sub(_amount);
+        return trolls[_trollNumber].power;
+    }
+    
+    function DecreaseSpeed(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].speed = trolls[_trollNumber].speed.sub(_amount);
+        troll_owner[_key][_trollNumber].speed = troll_owner[_key][_trollNumber].speed.sub(_amount);
+        return trolls[_trollNumber].speed;
+    }
+    
+    function DecreaseAgility(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].agility = trolls[_trollNumber].agility.sub(_amount);
+        troll_owner[_key][_trollNumber].agility = troll_owner[_key][_trollNumber].agility.sub(_amount);
+        return trolls[_trollNumber].agility;
+    }
+    
+    function DecreaseCleverness(address _key, uint256 _trollNumber, uint8 _amount) public returns (uint256) {
+        trolls[_trollNumber].cleverness = trolls[_trollNumber].cleverness.sub(_amount);
+        troll_owner[_key][_trollNumber].cleverness = troll_owner[_key][_trollNumber].cleverness.sub(_amount);
+        return trolls[_trollNumber].cleverness;
+    }
+
+    
+    function UpdateTrollType(address _key, uint256 _trollNumber) public returns (bool success) {
+        require (trolls[_trollNumber].troll_level >= max_troll_level);
+        if (trolls[_trollNumber].troll_level >= max_troll_level) {
+            trolls[_trollNumber].type_of = trolls[_trollNumber].type_of.add(1);
+            troll_owner[_key][_trollNumber].type_of = troll_owner[_key][_trollNumber].type_of.add(1);
         }
         return true;
-        
     }
     
-    function UpdateTypeOne() private {
-        for (uint256 i = 0; i < players.length; i++) {
-                if (players[i].key == msg.sender) {
-                    playerProfile[msg.sender].type1 = playerProfile[msg.sender].type1.add(1);
-                    players[i].type1 = players[i].type1.add(1);
-                    players[i].player_trolls.push(trolls.length);
-                } 
-            }
+    function DeadTroll(address _key, uint256 _trollNumber) public {
+        trolls[_trollNumber].playable = false;
+        troll_owner[_key][_trollNumber].playable = false;
     }
     
-    function UpdateTypeTwo() private {
-        for (uint256 i = 0; i < players.length; i++) {
-                if (players[i].key == msg.sender) {
-                    playerProfile[msg.sender].type2 = playerProfile[msg.sender].type2.add(1);
-                    players[i].type2 = players[i].type2.add(1);
-                    players[i].player_trolls.push(trolls.length);
-                }
-            }
+    function ReviveTroll(address _key, uint256 _trollNumber) public {
+        trolls[_trollNumber].playable = true;
+        troll_owner[_key][_trollNumber].playable = true;
     }
-    
-     function CheckPayment() private {
-        require (msg.value >= payment1);    
-        require (msg.value >= payment2);
-        require (msg.value >= payment3);
-        require (msg.value >= payment4);
-        require (msg.value >= payment5);
-    }
-    
     
 }
