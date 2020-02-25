0c9adcb78b587be8227128939e053bdbad5163b1
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-02-25 13:35:14 +1100

    Update Contract1.sol

--- Contract1.sol
+++ Contract1.sol
@@ -1,34 +1,32 @@
 pragma solidity ^0.5.1;
 
 import"./SafeMath.sol";
-// import"./Contract2.sol";
 
 contract TrollFactory {
     
     using SafeMath for uint32;
     using SafeMath for uint256;
     
-    uint128 internal total_trolls;
+    uint256 internal total_trolls;
     
     uint32 public troll_types;
     uint32 public max_troll_level = 50;
     address public contract_owner;
     bool public contract_active;
     
-    // Add stat for times levels has been lost. --- Done.
-    // NOTE: "Cleverness" stat has been taken out, remove functions.
+    
     struct Troll {
         string name;
-        uint32 type_of;
+        uint256 type_of;
         uint256 troll_level;
-        uint32 health;
+        uint256 health;
         uint256 lifes;
-        uint32 times_lost;
-        uint32 strength;
-        uint32 power;
-        uint32 speed;
-        uint32 agility;
-        uint128 troll_number;
+        uint256 times_lost;
+        uint256 strength;
+        uint256 power;
+        uint256 speed;
+        uint256 agility;
+        uint256 troll_number;
         address owner;
         bool playable;
     }
@@ -50,35 +48,35 @@ contract TrollFactory {
     }
     
     function Newtroll_T1(string memory _name) internal {
-       // total_trolls = trolls.length;
+        total_trolls.add(1);
         Troll memory newTroll = Troll(_name, 1, 1, 100, 10, 0, 0, 0, 0, 0, total_trolls, msg.sender, true);
         troll_owner[msg.sender][total_trolls] = newTroll;
         trolls.push(newTroll);
     }
     
     function Newtroll_T2(string memory _name) internal {
-       // total_trolls = trolls.length;
+        total_trolls.add(1);
         Troll memory newTroll = Troll(_name, 1, 1, 100, 20, 0, 0, 0, 0, 0, total_trolls, msg.sender, true);
         troll_owner[msg.sender][total_trolls] = newTroll;
         trolls.push(newTroll);
     }
     
     function Newtroll_T3(string memory _name) internal {
-       // total_trolls = trolls.length;
+        total_trolls.add(1);
         Troll memory newTroll = Troll(_name, 1, 1, 100, 30, 0, 0, 0, 0, 0, total_trolls, msg.sender, true);
         troll_owner[msg.sender][total_trolls] = newTroll;
         trolls.push(newTroll);
     }
     
     function Newtroll_T4(string memory _name) internal {
-    //    total_trolls = trolls.length;
+        total_trolls.add(1);
         Troll memory newTroll = Troll(_name, 1, 1, 100, 40, 0, 0, 0, 0, 0, total_trolls, msg.sender, true);
         troll_owner[msg.sender][total_trolls] = newTroll;
         trolls.push(newTroll);
     }
     
     function Newtroll_T5(string memory _name) internal {
-       // total_trolls = trolls.length;
+        total_trolls.add(1);
         Troll memory newTroll = Troll(_name, 1, 1, 100, 5, 0, 0, 0, 0, 0, total_trolls, msg.sender, true);
         troll_owner[msg.sender][total_trolls] = newTroll;
         trolls.push(newTroll);
@@ -95,8 +93,6 @@ contract TrollFactory {
             if (trolls[i].troll_number == _trollNumber) {
                 trolls[i].troll_level = trolls[i].troll_level.add(1);
                 troll_owner[_key][_trollNumber].troll_level = troll_owner[_key][_trollNumber].troll_level.add(1);
-                
-                // Just Added - Incrementation of 1 life with every level up.
                 troll_owner[_key][_trollNumber].lifes = troll_owner[_key][_trollNumber].lifes.add(1);
             }
         }
@@ -107,6 +103,7 @@ contract TrollFactory {
             if (trolls[i].troll_number == _trollNumber) {
                 trolls[i].troll_level = trolls[i].troll_level.sub(1);
                 troll_owner[_key][_trollNumber].troll_level = troll_owner[_key][_trollNumber].troll_level.sub(1);
+                troll_owner[_key][_trollNumber].lifes = troll_owner[_key][_trollNumber].lifes.sub(1);
             }
         }
     }
