193134e591648274317955c8833cf54b08443d8b
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-03-02 15:03:56 +1100

    Update Contract6.sol

--- Contract6.sol
+++ Contract6.sol
@@ -13,11 +13,27 @@ contract Marketplace {
     uint public _totalSales;
     address public _owner;
     
+    struct item {
+        uint _item_id;
+        uint _price;
+        uint _time_slot;
+        address _owner;
+        bool _sold;
+    }
+    
+    item[] public items;
+    
+    event NewItemForSale(uint _item_id, uint _price, uint _time_slot);
     event SaleFinalised(address _buyer, address _seller, uint _item, uint _price);
     
    function Buy(address _seller, uint _item, uint _price) public {
        erc20.Transfer(_seller, _price);
        _totalSales++;
        emit SaleFinalised(msg.sender, _seller, _item, _price);
+   }
+   
+   function Sell(uint _price, uint _time_slot) public {
+       items.push(item(items.length, _price, _time_slot, msg.sender, false));
+       emit NewItemForSale(items.length, _price, _time_slot);
    }
 }
