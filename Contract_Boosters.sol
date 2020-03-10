ab89eae91477d2d01f618899d62eae865ddd9cea
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2020-03-11 04:58:26 +1100

    Update Contract_Boosters.sol

--- Contract_Boosters.sol
+++ Contract_Boosters.sol
@@ -13,7 +13,13 @@ contract Boosters {
     uint public BOOSTER_P8 = 80;
     uint public BOOSTER_P9 = 90;
     uint public BOOSTER_P10 = 100;
+    uint public BOOSTER_P11 = 15;
+    uint public BOOSTER_P12 = 28;
+    uint public BOOSTER_P13 = 37;
+    uint public BOOSTER_P14 = 41;
+    uint public BOOSTER_P15 = 59;
     
+    uint public TOTAL_BOOSTERS = 0;
     
     struct booster1 {
         uint BOOSTER_P1;
@@ -56,28 +62,33 @@ contract Boosters {
     }
     
     struct booster11 {
+        uint BOOSTER_P11;
         uint BOOSTER_P1;
         uint BOOSTER_P10;
     }
     
     struct booster12 {
+        uint BOOSTER_P12;
         uint BOOSTER_P2;
         uint BOOSTER_P9;
         uint BOOSTER_P5;
     }
     
     struct booster13 {
+        uint BOOSTER_P13;
         uint BOOSTER_P3;
         uint BOOSTER_P5;
         uint BOOSTER_P4;
     }
     
     struct booster14 {
+        uint BOOSTER_P14;
         uint BOOSTER_P4;
         uint BOOSTER_P10;
     }
     
     struct booster15 {
+        uint BOOSTER_P15;
         uint BOOSTER_P10;
         uint BOOSTER_P9;
         uint BOOSTER_P8;
@@ -87,22 +98,33 @@ contract Boosters {
     uint[] public boosters;
     
     constructor () public {
-        booster_t[0] = 0;
         booster_t[1] = BOOSTER_P1;
-        booster_t[1] = BOOSTER_P1;
-         booster_t[1] = BOOSTER_P1;
-        booster_t[1] = BOOSTER_P1;
-        booster_t[1] = BOOSTER_P1;
-        booster_t[1] = BOOSTER_P1;
-        booster_t[1] = BOOSTER_P1;
-        booster_t[1] = BOOSTER_P1;
-        
+        booster_t[2] = BOOSTER_P2;
+        booster_t[3] = BOOSTER_P3;
+        booster_t[4] = BOOSTER_P4;
+        booster_t[5] = BOOSTER_P5;
+        booster_t[6] = BOOSTER_P6;
+        booster_t[7] = BOOSTER_P7;
+        booster_t[8] = BOOSTER_P8;
+        booster_t[9] = BOOSTER_P9;
+        booster_t[10] = BOOSTER_P10;
+        booster_t[11] = BOOSTER_P11;
+        booster_t[12] = BOOSTER_P12;
+        booster_t[13] = BOOSTER_P13;
+        booster_t[14] = BOOSTER_P14;
+        booster_t[0] = BOOSTER_P15;
     } 
     
-    function NewBooster() public {
-        
+    function NewBooster(uint _bn) public {
+        boosters.push(booster_t[_bn]);
+        TOTAL_BOOSTERS++;
     }
     
-    
+    function NewBoosterPack() public {
+        boosters.push(booster_t[1]);
+        boosters.push(booster_t[5]);
+        boosters.push(booster_t[9]);
+        TOTAL_BOOSTERS++;
+    }
     
 }
